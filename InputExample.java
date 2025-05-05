e1a7a862d1a73b290d4c485040e7c6008b8f009f
TerryBisieri <terryanne.kinanga@strathmore.edu>
2025-05-05 14:34:05 +0300

    Challenge 4

---
+++ InputExample.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class InputExample{
+  Scanner scanner = new Scanner(Syatem.in);
+
+  public void readNameAndAge(){
+    String name;
+    int age;
+    System.out.println("Name? ");
+    name=scanner.next();
+    System.out.println("Age? );
+    age=scanner.nextInt();
+
+    System.out.println("Hello "+name+ " , you are "+ age + "springs young ");
+
+    public static void main(String[] args){
+      (new InputExample()).readNameAndAge();
+    }
+    
+  }
+}
