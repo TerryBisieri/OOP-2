03dac53812cb17b185af94569fc768cc5d11d5a3
TerryBisieri <terryanne.kinanga@strathmore.edu>
2025-07-07 14:59:37 +0300

    Update Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+package Recursion;
+public class Factorial {
+    public static int factorial(int n){
+        //Base case
+        if (n==0 || n == 1){
+            return 1;
+        }
+        else{
+            //recursive case: n* factorial of(n-1)
+            return n * factorial(n-1);
+        }
+    }
+    public static void main(String[] args){
+        int result = factorial(5); // Calculates the factorial of 5
+        System.out.println("Factorial :"+ result);
+    }
+}
